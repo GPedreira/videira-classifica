751937d9bcaea292da18f94d6ffabb813f32a576
GPedreira <142950870+GPedreira@users.noreply.github.com>
2025-10-27 08:57:32 -0300

    app.py

--- app.py
+++ app.py
@@ -9,9 +9,9 @@ import plotly.express as px
 
 @st.cache_resource
 def carrega_modelo():
-    # https://drive.google.com/file/d/1VPpwL8cagLfP8j6smglsCrarV0182Jkh/view?usp=drive_link
-    #https://drive.google.com/file/d/16YdJi8bAMXMYaD-z1suNFX6nA2PwlDJp/view?usp=sharing
+    # https://drive.google.com/file/d/16YdJi8bAMXMYaD-z1suNFX6nA2PwlDJp/view?usp=sharing
     url = 'https://drive.google.com/uc?id=16YdJi8bAMXMYaD-z1suNFX6nA2PwlDJp'
+    
     gdown.download(url, 'modelo_quantizado16bits.tflite')
     interpreter = tf.lite.Interpreter(model_path='modelo_quantizado16bits.tflite')
     interpreter.allocate_tensors()
@@ -35,13 +35,24 @@ def carrega_imagem():
 
 def previsao(interpreter,image):
 
-    #código omitido
-    
+  # Obtém detalhes dos tensores de entrada e saída
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+
+    # Define o tensor de entrada para o modelo
+    interpreter.set_tensor(input_details[0]['index'], image)
+
+    # Executa a inferência
+    interpreter.invoke()
+
+    # Obtém a saída do modelo
+    output_data = interpreter.get_tensor(output_details[0]['index'])
+    classes = ['BlackMeasles', 'BlackRot', 'HealthyGrapes', 'LeafBlight']
     df = pd.DataFrame()
     df['classes'] = classes
     df['probabilidades (%)'] = 100*output_data[0]
-    
-    fig = px.bar(df,y='classes',x='probabilidades (%)',  orientation='h', text='probabilidades (%)', title='Probabilidade de Classes de Doenças em Uvas')
+    fig = px.bar(df, y='classes', x='probabilidades (%)', orientation='h', text='probabilidades (%)',
+             title='Probabilidade de Classes de Doenças em Uvas')
     st.plotly_chart(fig)
 
 def main():
